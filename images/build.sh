28c26470eb05748caa492af570de26588edbc036
c0mmando <c0mmando@pm.me>
2023-05-26 21:16:09 +0000

    add image dockerfiles

--- images/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build ./synapse-captcha/
-docker build ./mjolnir/
-docker build ./pantalaimon/
-docker build ./synapse-docker/
-docker build ./synapse-worker-docker/
-docker build ./matrix-dimension/
